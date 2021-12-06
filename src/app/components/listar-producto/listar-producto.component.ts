cb92aca066bf3657c053e70ce4ec84907a8fd177
Haresca <thehespway@gmail.com>
2021-12-05 23:51:21 -0500

    initial commit

---
+++ src/app/components/listar-producto/listar-producto.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
+
+@Component({
+  selector: 'app-listar-producto',
+  templateUrl: './listar-producto.component.html',
+  styleUrls: ['./listar-producto.component.css']
+})
+export class ListarProductoComponent implements OnInit {
+  listProductos: Producto [] = [];
+
+  constructor(private _productoService: ProductoService,
+    private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+    this.obtenerProductos();
+  }
+
+  obtenerProductos() {
+    this._productoService.getproductos().subscribe(data => {
+      console.log(data);
+      this.listProductos = data;
+    }, error => {
+      console.log(error);
+    })
+  }
+  eliminarProducto(id:any){
+    this._productoService.eliminarProductos(id).subscribe(data => {
+      this.toastr.error('este producto fue eliminado', 'producto eliminado');
+      this.obtenerProductos();
+    }, error => {
+      console.log(error);
+    })
+  }
+}
